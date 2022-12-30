a7131e892dbda9096748b81aab05a18ec3e7b796
Patato <pathonbusiness@gmail.com>
2022-12-30 14:26:47 +0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Node + TypeScript
+
+A simple NodeJS + TypeScript setup
+
+> made from [Fireship's Video](https://youtu.be/H91aqUHn8sE)
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import twitter from "starlordtwitterscraper";
+
+const d = await twitter("https://twitter.com/bemyrainbow23");
+await console.log(d);
